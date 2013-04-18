1075690168115fc1cb409bfecc6d6b1aca52cd53
Benjamin Cabé <BCabe@sierrawireless.com>
2013-04-18 13:52:12 +0200

    First version of a Ruby robot

---
+++ jenkins-robot.rb
@@ -0,0 +1,44 @@
+require 'awesome_print'
+require 'net/http'
+require 'net/https'
+require 'openssl'
+require 'uri'
+require 'json'
+
+uri = URI.parse("https://jenkins/platform-new/api/json?pretty=true")
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE # read into this
+@data = http.get(uri.request_uri)
+
+result = JSON.parse(@data.body)
+
+#ap result
+
+http = Net::HTTP.new("m2m.eclipse.org")
+
+colors = {}
+
+colors['grey'] = { :red => 100, :green => 100, :blue => 100}
+colors['aborted'] = { :red => 100, :green => 100, :blue => 100}
+colors['yellow'] = { :red => 255, :green => 255, :blue => 0}
+colors['blue'] = { :red => 0, :green => 0, :blue => 255}
+colors['red'] = { :red => 255, :green => 0, :blue => 0}
+
+colors['aborted_anime'] = { :red => 100, :green => 100, :blue => 100, :blink => true}
+colors['yellow_anime'] = { :red => 255, :green => 255, :blue => 0, :blink => true}
+colors['blue_anime'] = { :red => 0, :green => 0, :blue => 255, :blink => true}
+colors['red_anime'] = { :red => 255, :green => 0, :blue => 0, :blink => true}
+
+values = []
+result['jobs'].each do |j|
+#	ap j['color']
+	values << colors[j['color']]
+end
+
+settings = { :settings => [ { :key => "leds.writePixels", :value => values } ] }
+#ap settings.to_json
+
+request = Net::HTTP::Post.new("/m3da/data/jaxcon-demo")
+request.body = settings.to_json
+response = http.request(request)
