f1295c2985b15e55219a9feb6d68bf9ac3bd636b
Benjamin Cabé <BCabe@sierrawireless.com>
2013-04-20 12:17:05 +0200

    adding EPL license headers

--- guirlande.js
+++ guirlande.js
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Sébastien Moran and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Sébastien Moran - initial API and implementation
+ *******************************************************************************/
+
 var request = require('request');
 var _ = require('underscore');
 var nconf = require('nconf');
--- jenkins-robot.rb
+++ jenkins-robot.rb
@@ -1,3 +1,14 @@
+###############################################################################
+# Copyright (c) 2013 Benjamin Cabé and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#     Benjamin Cabé - initial API and implementation
+###############################################################################
+
 require 'awesome_print'
 require 'net/http'
 require 'net/https'
